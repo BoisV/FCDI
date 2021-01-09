a97300f3a6ccaec19466a910cf71d4fd34ab550a
bois <next.to.you@qq.com>
2021-01-09 22:35:21 +0800

    update model.py

--- lib/model/model.py
+++ lib/model/model.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from torch import functional as F
+
+
 class FeatrueExtractor(nn.Module):
     def __init__(self):
         super(FeatrueExtractor, self).__init__()
@@ -12,19 +14,22 @@ class FeatrueExtractor(nn.Module):
             nn.MaxPool2d(kernel_size=2)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=96, out_channels=64, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=96, out_channels=64,
+                      kernel_size=5, stride=1),
             nn.BatchNorm2d(num_features=64),
             nn.Tanh(),
             nn.MaxPool2d(kernel_size=2)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=64, out_channels=64,
+                      kernel_size=5, stride=1),
             nn.BatchNorm2d(num_features=64),
             nn.Tanh(),
             nn.MaxPool2d(kernel_size=2)
         )
-        self.conv5=nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=1),
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128,
+                      kernel_size=1, stride=1),
             nn.BatchNorm2d(num_features=128),
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2)
@@ -38,8 +43,7 @@ class FeatrueExtractor(nn.Module):
             nn.Tanh()
         )
 
-    
-    def forward(self, x):
+    def cal(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
@@ -50,9 +54,43 @@ class FeatrueExtractor(nn.Module):
         x = self.fc2(x)
         return x
 
+    def forward(self, x1, x2):
+        x1 = self.cal(x1)
+        x2 = self.cal(x2)
+        return x1, x2
+
+
+class SimilarityNet(nn.Module):
+    def __init__(self):
+        super(SimilarityNet, self).__init__()
+        self.fcA = nn.Sequential(
+            nn.Linear(in_features=200, out_features=2048, bias=True),
+            nn.ReLU()
+        )
+        self.fcB = nn.Sequential(
+            nn.Linear(in_features=2048*3, out_features=64, bias=True),
+            nn.ReLU()
+        )
+        self.sim_neuron = nn.Sequential(
+            nn.Linear(in_features=64, out_features=1, bias=True),
+            # nn.Softmax(dim=1)
+        )
+
+    def forward(self, x1, x2):
+        x1_inter = self.fcA(x1)
+        x2_inter = self.fcA(x2)
+        X = torch.cat([x1_inter, x2_inter, x1_inter*x2_inter], dim=1).flatten(start_dim=1)
+        X = self.fcB(X)
+        return self.sim_neuron(X)
+
+
 if __name__ == "__main__":
-    model = FeatrueExtractor()
-    print(model)
-    X = torch.randint(0, 256, size=[1,3, 256, 256], dtype=torch.float32)
-    y = model(X)
-    print(y.shape)
+    featureEx = FeatrueExtractor()
+    similarityNet = SimilarityNet()
+    print(featureEx)
+    X1 = torch.randint(0, 256, size=[1, 3, 256, 256], dtype=torch.float32)
+    X2 = torch.randint(0, 256, size=[1, 3, 256, 256], dtype=torch.float32)
+    X1, X2 = featureEx(X1, X2)
+    print(X1.shape, X2.shape)
+    y = similarityNet(X1, X2)
+    print(y)
